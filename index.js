d7df7ec04ce4657d21363aa39530b7bc5f0467f8
Mitani Takashi <mitani.t@ktk.gr.jp>
2015-03-18 14:11:00 +0900

    flags用の定数を定義

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ module.exports.SMFIS_DISCARD = require('./lib/constants').SMFIS.DISCARD;
 module.exports.SMFIS_ACCEPT = require('./lib/constants').SMFIS.ACCEPT;
 module.exports.SMFIS_TEMPFAIL = require('./lib/constants').SMFIS.TEMPFAIL;
 module.exports.SMFIS_ALL_OPTS = require('./lib/constants').SMFIS.ALL_OPTS;
+
+module.exports.SMFIF_ADDHDRS = require('./lib/constants').SMFIF.ADDHDRS;
+module.exports.SMFIF_CHGHDRS = require('./lib/constants').SMFIF.CHGHDRS;
+module.exports.SMFIF_CHGBODY = require('./lib/constants').SMFIF.CHGBODY;
+module.exports.SMFIF_ADDRCPT = require('./lib/constants').SMFIF.ADDRCPT;
+module.exports.SMFIF_ADDRCPT_PAR = require('./lib/constants').SMFIF.ADDRCPT_PAR;
+module.exports.SMFIF_DELRCPT = require('./lib/constants').SMFIF.DELRCPT;
+module.exports.SMFIF_QUARANTINE = require('./lib/constants').SMFIF.QUARANTINE;
+module.exports.SMFIF_CHGFROM = require('./lib/constants').SMFIF.CHGFROM;
+module.exports.SMFIF_SETSYMLIST = require('./lib/constants').SMFIF.SETSYMLIST;
