186cb5f3428e4183ef0eaa84a0fcc7f64f5e64fa
Mitani Takashi <mitani.t@ktk.gr.jp>
2015-03-18 13:06:13 +0900

    公開する定数の定義の仕方を変更

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 module.exports.createMilter = require('./lib/milter').createMilter;
+
 module.exports.SMFI_VERSION = require('./lib/constants').SMFI_VERSION;
-module.exports.SMFIS = require('./lib/constants').SMFIS;
+
+module.exports.MI_SUCCESS = require('./lib/constants').MI_SUCCESS;
+module.exports.MI_FAILURE = require('./lib/constants').MI_FAILURE;
+
+module.exports.SMFIS_CONTINUE = require('./lib/constants').SMFIS.CONTINUE;
+module.exports.SMFIS_REJECT = require('./lib/constants').SMFIS.REJECT;
+module.exports.SMFIS_DISCARD = require('./lib/constants').SMFIS.DISCARD;
+module.exports.SMFIS_ACCEPT = require('./lib/constants').SMFIS.ACCEPT;
+module.exports.SMFIS_TEMPFAIL = require('./lib/constants').SMFIS.TEMPFAIL;
+module.exports.SMFIS_ALL_OPTS = require('./lib/constants').SMFIS.ALL_OPTS;
